18859b36caac1d7921a68b62334dc4e179046639
Mackenzie Card <kenziecard@gmail.com>
2022-11-02 11:05:13 -0400

    Added to About Me + Contact Sections

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -5,20 +5,29 @@ const AboutMe = () => {
     return (
         <Wrapper> 
         <Title>About Me</Title>
-        <Info> Hi! I'm Mackenzie.</Info>
         <Blurb>
-            <div>
+        <Info> Hi! I'm Mackenzie.</Info>
+            <Intro>
             I am a recent graduate of Concordia University's Full Stack Web Development Bootcamp.
-            </div>
-            <div>
-            I became interested in web development through a family member, and began to teach myself the basics in January 2022.
-            </div>
-            <div>
+            </Intro>
+            <SecondPart>
+            I became interested in coding and web development because of the creative potential I saw in the field. 
+            As I began to teach myself the basics of HTML and CSS, I saw just how boundless the innovation 
+            of web development, web design and UX design could truly be.
+            </SecondPart> 
+            <ThirdPart>
+            Through my educational journey with web development I have become interested in making 
+            applications that offer the best user experiences possible. I am now taking the web 
+            development skills I have gained and applying them to my new educational journey with UX 
+            design. I am looking for opportunities to put my skills to use on projects with similar 
+            goals of creating original, thoughtful and unique user experiences.
+            </ThirdPart>
+            <FourthPart>
             I am still learning every day, and continuing to grow my portfolio as I go along. 
-            </div>
-            <div>
-                I hope you enjoy taking a look at what I have been able to create in the last few months as a student and enthusiast!
-            </div>
+            </FourthPart>
+            <FifthPart>
+            I hope you enjoy taking a look at what I have been able to create in the last few months as a student and enthusiast!
+            </FifthPart>
         </Blurb>
         </Wrapper>
     )
@@ -28,12 +37,40 @@ export default AboutMe;
 
 const Wrapper = styled.div`
 font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+
 `; 
 
 const Title = styled.div`
 font-weight: bold;
+margin-bottom: 10px;
 `; 
 
-const Info = styled.div``;
+const Info = styled.div`
+margin-bottom: 10px;
+`;
 
-const Blurb = styled.div``; 
+const Blurb = styled.div`
+display:flex;
+flex-direction: column;
+align-items: center;
+justify-content:center;
+width: 500px;
+`; 
+
+const Intro = styled.div`
+margin-bottom: 10px;
+`; 
+
+const SecondPart = styled.div`
+margin-bottom: 10px;
+`; 
+
+const ThirdPart = styled.div`
+margin-bottom: 10px;
+`; 
+
+const FourthPart = styled.div`
+margin-bottom: 10px;
+`; 
+
+const FifthPart = styled.div``; 
--- client/src/ContactMe.js
+++ client/src/ContactMe.js
@@ -1,10 +1,46 @@
 import React from "react";
 import styled from "styled-components";
+import {FiMail} from 'react-icons/fi'; 
+import {BsTelephone} from 'react-icons/bs'; 
+import {BsGithub} from 'react-icons/bs'; 
+import {BsLinkedin} from 'react-icons/bs'; 
 
 const ContactMe = () => {
     return (
-        <h1>Contact Me</h1>
+        <Wrapper>
+        <Title>Contact Me</Title>
+        <Email><FiMail/> kenziecard@gmail.com </Email>
+        <Phone><BsTelephone/> +1 (514) 299-7324</Phone>
+        <GitHub><BsGithub/> github.com/MackenzieCard </GitHub>
+        <LinkedIn><BsLinkedin/> linkedin.com/in/mackenziecard/ </LinkedIn>
+        </Wrapper>
     )
 }
 
 export default ContactMe; 
+
+const Wrapper = styled.div`
+font-family: var(--font-body);
+`; 
+
+const Title = styled.div`
+font-size: 22px;
+font-weight: bold;
+font-family: var(--font-body);
+`; 
+
+const Email = styled.div`
+
+`;
+
+const Phone = styled.div`
+
+`; 
+
+const GitHub = styled.div`
+
+`; 
+
+const LinkedIn = styled.div`
+
+`; 
